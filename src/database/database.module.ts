ff6b944eae3698be611e2f0109e04183c6166cf2
Chris Tse <ctse@rent.com>
2024-10-21 16:27:00 -0500

    create hono app

--- src/database/database.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { dataSourceOptions } from '../../db/data-source'
-
-@Module({
-  imports: [TypeOrmModule.forRoot({ ...dataSourceOptions, synchronize: process.env.NODE_ENV !== 'production' })],
-})
-export class DatabaseModule {}
--- src/work-history/dto/update-work-history.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateWorkHistoryDto } from './create-work-history.dto';
-
-export class UpdateWorkHistoryDto extends PartialType(CreateWorkHistoryDto) {}
